8ed3a0664025cd66ec84daed67257e10e4d79c05
bumpy_lumps <alexander.fulop.art@gmail.com>
2023-11-06 18:04:56 -0500

    added recaptcha to client, still needs server validation

--- controllers/contact.js
+++ controllers/contact.js
@@ -1,5 +1,7 @@
-const { Resend } = require('resend')
-
+const { Resend } = require('resend');
+const request = require('request');
+const axios = require("axios")
+const RECAPTCHA_SECRET_KEY = `${process.env.RECAPTCHA_SECRET_KEY}`
 
 
 module.exports = {
@@ -14,46 +16,53 @@ module.exports = {
         }
     },
 
-    submitFeedback: async (req,res) => {
+    // getValidation: async (request, response) =>{ 
+        // const { ValidationToken } = request.body;
+        // const { data } = await axios.post(`https://www.google.com/recaptcha/api/siteverify?secret=${RECAPTCHA_SECRET_KEY}&response=${ValidationToken}`);
+// 
+// 
+        // console.log(data)
+        // if(data.success){
+            // return true;
+        // } else {
+            // return false;
+        // }
+    // },
+
+    submitFeedback: async (req, res) => {
         const resend = new Resend(`${process.env.RESEND_API_KEY}`);
         
-        let message = {
-            name: req.body.name,
-            email: req.body.email,
-            subject: req.body.subject,
-            message: req.body.message
-        }
-        
-        try {
-            
-             const data = await resend.sendEmail({
-               from: "FanMail@quiteunusualpod.com",
-               to: "quiteunusualpod@gmail.com",
-               subject: req.body.subject,
-               text: `${req.body.name} sent you fanmail from ${req.body.email}! 
-               Their message: ${req.body.message}`,
-             });
 
-            res.redirect('/thankYou')
-        
-          }
-          catch(e) {
+        // let message = {
+        //     name: req.body.name,
+        //     email: req.body.email,
+        //     subject: req.body.subject,
+        //     message: req.body.message
+        // }
+
+        // let validation = getValidation();
+
+        // if(validation === true){  
+            try {  
+                const data = await resend.sendEmail({
+                   from: "FanMail@quiteunusualpod.com",
+                   to: "bumpsites@gmail.com",
+                   subject: req.body.subject,
+                   text: `${req.body.name} sent you fanmail from ${req.body.email}! 
+                   Their message: ${req.body.message}`,
+                 });
+
+                res.redirect('/thankYou')
+           
+            } catch(e) {
             console.log(e)
             res.status(400).json(e);
-          }
-
-        // try {
+          } 
         
-
-        //     console.log(message);
-        //     //res.status(200).json(data);
-        //     res.redirect('/')
-
-        // } catch(err) {
-        //     console.log('I\'m not working' + ' ' +  err )
-
+        // } else {
+                // console.log('it\'s broken')
+            // }
         // }
+                
     } 
-
-
 }
